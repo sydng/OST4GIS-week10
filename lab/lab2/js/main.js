deffafd1dcd0a907fe467989a346397e7197f66b
sydng <35381098+sydng@users.noreply.github.com>
2018-04-02 12:28:00 -0400

    Carto Lab

--- lab/lab2/js/main.js
+++ lab/lab2/js/main.js
@@ -1,7 +1,7 @@
 // Leaflet map setup
 var map = L.map('map', {
-  center: [39.923004, -75.183477],
-  zoom: 13
+  center: [39.971322, -75.159603],
+  zoom: 11
 });
 
 var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
@@ -28,9 +28,10 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolab
 // To add visualizations created with the Analysis feature, you will need to export the data to a
 // GeoJSON. From there, you can either import the GeoJSON to Carto or use Leaflet's L.geoJson.
 
+/*
+I exported out from carto in GeoJSON format.
 var cartoUserName = 'jfreink';
 var cartoVizId = '1f7c9af2-1088-11e7-9c60-0e05a8b3e3d7';
-
 var layerUrl = 'https://'+cartoUserName+'.carto.com/api/v2/viz/'+cartoVizId+'/viz.json';
 
 cartodb.createLayer(map, layerUrl)
@@ -39,3 +40,17 @@ cartodb.createLayer(map, layerUrl)
   }).on('error', function(err) {
     console.log(err);
   });
+*/
+
+var geojsonURL = 'https://raw.githubusercontent.com/sydng/OST4GIS-week10/master/inlets.geojson';
+var geojsonURL2 = 'https://raw.githubusercontent.com/sydng/OST4GIS-week10/master/inlets_green.geojson';
+var data;
+
+$(document).ready(function() {
+  $.ajax(geojsonURL2).done(function(info) {
+    data = JSON.parse(info);
+    var forMapping = L.geoJson(data, {
+      color: 'green'
+    }).addTo(map);
+  });
+});
